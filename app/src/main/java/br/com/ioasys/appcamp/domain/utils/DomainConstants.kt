4ebb08d30df2238dba6819dfecda3080b8efdcbd
Daniel B. Alves <71525341+danbalves@users.noreply.github.com>
2022-04-06 17:09:29 -0300

    Creating email regex const

---
+++ app/src/main/java/br/com/ioasys/appcamp/domain/utils/DomainConstants.kt
@@ -0,0 +1,6 @@
+package br.com.ioasys.appcamp.domain.utils
+
+object DomainConstants {
+
+    const val EMAIL_REGEX = "^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})"
+}
--- app/src/main/java/br/com/ioasys/appcamp/presentation/viewmodel/SingUpViewModel.kt
+++ app/src/main/java/br/com/ioasys/appcamp/presentation/viewmodel/SingUpViewModel.kt
@@ -4,18 +4,48 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.ioasys.appcamp.domain.model.SingUpItems
+import br.com.ioasys.appcamp.domain.usecase.SingUpUseCase
 import br.com.ioasys.appcamp.utils.ViewState
+import br.com.ioasys.appcamp.utils.postError
+import br.com.ioasys.appcamp.utils.postLoading
 
-class SingUpViewModel(): ViewModel() {
+class SingUpViewModel(
+    private val singUpUseCase: SingUpUseCase
+): ViewModel() {
 
     private var _singUpViewState = MutableLiveData<ViewState<List<SingUpItems>>>()
     val singUpViewState = _singUpViewState as LiveData<ViewState<List<SingUpItems>>>
 
-    var genre: String = null ?: ""
-        private set
+    private var _genre: String = null ?: ""
+    private val genreValue: String = _genre
 
 
     fun setGenre(genreValue: String){
-        genre = genreValue
+        _genre = genreValue
     }
+
+    fun singUp(
+        user: String,
+        email: String,
+        password: String,
+        confirmPassword: String
+    ){
+        _singUpViewState.postLoading()
+        singUpUseCase(
+            params = SingUpUseCase.Params(
+                user = user,
+                email = email,
+                password = password,
+                confirmPassword = confirmPassword,
+                genre = genreValue
+            ),
+            onSuccess = {},
+            onError = {
+                _singUpViewState.postError(it)
+            }
+        )
+    }
+
+
+
 }
