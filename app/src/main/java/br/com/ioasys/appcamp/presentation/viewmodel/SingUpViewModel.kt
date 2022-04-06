786d8faa58616ece67391b448165e0209c10b1ed
Daniel B. Alves <71525341+danbalves@users.noreply.github.com>
2022-04-06 15:09:48 -0300

    Creating ViewState and LiveData

--- app/src/main/java/br/com/ioasys/appcamp/presentation/viewmodel/SingUpViewModel.kt
+++ app/src/main/java/br/com/ioasys/appcamp/presentation/viewmodel/SingUpViewModel.kt
@@ -1,11 +1,15 @@
 package br.com.ioasys.appcamp.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.com.ioasys.appcamp.domain.model.SingUpItems
+import br.com.ioasys.appcamp.utils.ViewState
 
 class SingUpViewModel(): ViewModel() {
 
-//    private var _singUpViewState = MutableLiveData<ViewState<List<SingUpItems>>>()
-//    val singUpViewState = _singUpViewState as LiveData<ViewState<List<SingUpItems>>>
+    private var _singUpViewState = MutableLiveData<ViewState<List<SingUpItems>>>()
+    val singUpViewState = _singUpViewState as LiveData<ViewState<List<SingUpItems>>>
 
     var genre: String = null ?: ""
         private set
