3dd8c8e23d5cca1814f43442886199fe830b53de
Fabiana Farias <farias.fabianam@gmail.com>
2022-04-07 11:15:31 -0300

    Making some adjusts in fragment login

--- app/src/main/java/br/com/ioasys/appcamp/di/PresentationModule.kt
+++ app/src/main/java/br/com/ioasys/appcamp/di/PresentationModule.kt
@@ -1,9 +1,12 @@
 package br.com.ioasys.appcamp.di
 
+import br.com.ioasys.appcamp.presentation.viewmodel.LoginViewModel
 import br.com.ioasys.appcamp.presentation.viewmodel.SingUpViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val PresentationModule = module {
     viewModel { SingUpViewModel(get()) }
+    viewModel { LoginViewModel(get()) }
+
 }
