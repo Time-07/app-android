30d562297aad8f2bdc1e14848231071c3706d280
Fabiana Farias <farias.fabianam@gmail.com>
2022-04-12 15:35:18 -0300

    Adding LoginRemoteDatasource

--- app/src/main/java/br/com/ioasys/appcamp/data/repositories/LoginRepositoryImpl.kt
+++ app/src/main/java/br/com/ioasys/appcamp/data/repositories/LoginRepositoryImpl.kt
@@ -1,11 +1,17 @@
 package br.com.ioasys.appcamp.data.repositories
 
+import br.com.ioasys.appcamp.data.datasource.local.LoginLocalDataSource
+import br.com.ioasys.appcamp.data.datasource.remote.LoginRemoteDataSource
 import br.com.ioasys.appcamp.domain.model.User
 import br.com.ioasys.appcamp.domain.respositories.LoginRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-class LoginRepositoryImpl : LoginRepository {
+class LoginRepositoryImpl(
+    private val loginRemoteDataSource: LoginRemoteDataSource,
+    private val loginLocalDataSource: LoginLocalDataSource
+
+) : LoginRepository {
 
     override fun login(
         email: String,
