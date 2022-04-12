7843f839ac17b4095829721861ea321965aa8f0c
Fabiana Farias <farias.fabianam@gmail.com>
2022-04-12 16:51:32 -0300

    Adding dataLocalModule

---
+++ app/src/main/java/br/com/ioasys/appcamp/di/dataLocalModule.kt
@@ -0,0 +1,13 @@
+package br.com.ioasys.appcamp.di
+
+import br.com.ioasys.appcamp.data.data_local.utils.SharedPreferencesHelper
+import br.com.ioasys.appcamp.data.datasource.local.LoginLocalDataSource
+import br.com.ioasys.appcamp.data_local.datasource.LoginLocalDataSourceImpl
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+val dataLocalModule = module {
+
+    single { SharedPreferencesHelper(androidContext()) }
+    single<LoginLocalDataSource> { LoginLocalDataSourceImpl( get()) }
+}
