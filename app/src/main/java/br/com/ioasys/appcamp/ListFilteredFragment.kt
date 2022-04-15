10d82ba802bd73b97dbcb14d659b157ea6ae9182
Daniel B. Alves <71525341+danbalves@users.noreply.github.com>
2022-04-15 12:53:44 -0300

    Creating a new listFilteredFragment

--- app/src/main/java/br/com/ioasys/appcamp/ListFilteredFragment.kt
+++ app/src/main/java/br/com/ioasys/appcamp/ListFilteredFragment.kt
@@ -1,20 +1,19 @@
-package br.com.ioasys.appcamp.presentation.ui.fragments
+package br.com.ioasys.appcamp
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import br.com.ioasys.appcamp.R
 
-class ListFragment : Fragment() {
+
+class ListFilteredFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_default_list, container, false)
+        return inflater.inflate(R.layout.fragment_list_filtered, container, false)
     }
 
 }
