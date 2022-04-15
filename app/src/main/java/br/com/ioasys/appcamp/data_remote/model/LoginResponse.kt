e819d4dc1890640e4259bf63970e719a72a6f4f2
Fabiana Farias <farias.fabianam@gmail.com>
2022-04-14 21:05:51 -0300

    wip

--- app/src/main/java/br/com/ioasys/appcamp/data_remote/model/LoginResponse.kt
+++ app/src/main/java/br/com/ioasys/appcamp/data_remote/model/LoginResponse.kt
@@ -3,12 +3,8 @@ package br.com.ioasys.appcamp.data_remote.model
 import com.google.gson.annotations.SerializedName
 
 data class LoginResponse(
-    @SerializedName("name")
-    val name: String,
-    @SerializedName("birthdate")
-    val birthdate: String,
-    @SerializedName("gender")
-    val gender: String,
-    @SerializedName("id")
-    val id: String
+    @SerializedName("userRole")
+    val role: String,
+    @SerializedName("access_token")
+    val accessToken: String
 )
--- app/src/main/java/br/com/ioasys/appcamp/presentation/viewmodel/LoginViewModel.kt
+++ app/src/main/java/br/com/ioasys/appcamp/presentation/viewmodel/LoginViewModel.kt
@@ -3,18 +3,25 @@ package br.com.ioasys.appcamp.presentation.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import br.com.ioasys.appcamp.domain.usecase.LoginUseCase
 import br.com.ioasys.appcamp.util.ViewState
 import br.com.ioasys.appcamp.util.postError
 import br.com.ioasys.appcamp.util.postNeutral
 import br.com.ioasys.appcamp.util.postSuccess
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.core.parameter.parametersOf
 
-class LoginViewModel(
-    private val loginUseCase: LoginUseCase
-) : ViewModel() {
+class LoginViewModel
+    : ViewModel(), KoinComponent {
 
     private val _loggedUserViewState = MutableLiveData<ViewState<String>>()
     val loggedUserViewState = _loggedUserViewState as LiveData<ViewState<String>>
+
+    private val loginUseCase: LoginUseCase by inject {
+        parametersOf(viewModelScope)
+    }
 
     fun login(email: String, password: String) {
         loginUseCase(
