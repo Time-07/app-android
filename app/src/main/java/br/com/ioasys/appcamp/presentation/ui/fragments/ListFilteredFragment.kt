d822696f92e45c0d339214eef459f1a84d954d14
Daniel B. Alves <71525341+danbalves@users.noreply.github.com>
2022-04-16 18:07:20 -0300

    wip

--- app/src/main/java/br/com/ioasys/appcamp/presentation/ui/fragments/ListFilteredFragment.kt
+++ app/src/main/java/br/com/ioasys/appcamp/presentation/ui/fragments/ListFilteredFragment.kt
@@ -5,16 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import br.com.ioasys.appcamp.R
+import br.com.ioasys.appcamp.databinding.FragmentListFilteredBinding
 
 
 class ListFilteredFragment : Fragment() {
 
+    private var _binding: FragmentListFilteredBinding? = null
+    private val binding: FragmentListFilteredBinding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list_filtered, container, false)
+    ): View = FragmentListFilteredBinding.inflate(
+        inflater, container, false
+    ).apply {
+        _binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListeners()
+    }
+
+    private fun setListeners(){
+        binding.listFilteredReturnFilterButton.setOnClickListener {
+            findNavController().navigate(
+                ListFilteredFragmentDirections.actionListFilteredFragmentToSearchFragment()
+            )
+        }
+        binding.listFilteredCloseButton.setOnClickListener {
+            findNavController().navigate(
+                ListFilteredFragmentDirections.actionListFilteredFragmentToListFragment()
+            )
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
