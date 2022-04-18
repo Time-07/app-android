638c42a6bf767a3aeeef0808402f61686283a67a
Fabiana Farias <farias.fabianam@gmail.com>
2022-04-18 18:21:25 -0300

    Making the name split logic

--- app/src/main/java/br/com/ioasys/appcamp/presentation/ui/fragments/ProfessionalProfileFragment.kt
+++ app/src/main/java/br/com/ioasys/appcamp/presentation/ui/fragments/ProfessionalProfileFragment.kt
@@ -5,17 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import br.com.ioasys.appcamp.R
-
+import br.com.ioasys.appcamp.databinding.FragmentProfessionalProfileBinding
 
 
 class ProfessionalProfileFragment : Fragment() {
 
+    private var _binding: FragmentProfessionalProfileBinding? = null
+    private val binding: FragmentProfessionalProfileBinding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_professional_profile, container, false)
+    ): View = FragmentProfessionalProfileBinding.inflate(
+        inflater, container, false
+    ).apply {
+        _binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListeners()    }
+
+
+    private fun setListeners() {
+        binding.professionalFirstName.text = getFirstName("Fabiana Farias")
+        binding.professionalLastName.text = getLastName("Fabiana Farias")    }
+
+
+    private fun getFirstName(fullName: String?): String? {
+        val index = fullName?.lastIndexOf(' ')
+        return if (index == -1) {
+            fullName
+        } else index?.let { it1 -> fullName.substring(0, it1) }
+    }
+
+    private fun getLastName(fullName: String?): String? {
+        val index = fullName?.lastIndexOf(' ')
+        return if (index == -1) {
+            null
+        } else return index?.plus(1)?.let { it1 -> fullName.substring(it1) }
     }
 }
